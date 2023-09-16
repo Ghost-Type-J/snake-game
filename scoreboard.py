777619840000c2005cccd9c63645d5e44e98b387
ghost-type-j <107253394+ghost-type-j@users.noreply.github.com>
2023-09-16 15:26:21 +0100

    Added high score functionality using the "open" function

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
@@ -7,22 +8,27 @@ class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.hideturtle()
         self.color("white")
         self.penup()
         self.setposition(x=0, y=270)
-        self.score_count = 0
-        self.write(arg=f"Score: {self.score_count}", align=ALIGNMENT, font=FONT)
+        self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(arg=f"Score: {self.score_count}", align=ALIGNMENT, font=FONT)
-
-
-    def game_over(self):
-        self.goto(0,0)
-        self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
-
-    def points_up(self):
         self.clear()
-        self.score_count += 1
+        self.write(arg=f"Score: {self.score} High score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def high_reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
         self.update_scoreboard()
